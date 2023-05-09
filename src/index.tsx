e291ff18e4c485d7aed57cc061afb95d37bc7e07
jaegwans <skhu201811140@gmail.com>
2023-05-09 09:53:32 +0900

    feat: setting redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,22 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+//교수학습개발센터 승연관 2층 김혜란
+
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <App />
+            <Provider store={store}>
+                <GlobalStyle />
+                <App />
+            </Provider>
         </ThemeProvider>
     </React.StrictMode>
 );
