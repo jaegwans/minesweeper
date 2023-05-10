cbdef5f1ae513b32fe5032ff9de5f831bd67a33f
jaegwans <skhu201811140@gmail.com>
2023-05-10 15:45:01 +0900

    feat: 난이도 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeCells, ICell } from './app/lib/func/funcs';
 import styled from 'styled-components';
-
+// export interface ICell {
+//     flag: boolean;
+//     visible: boolean;
+//     value: number;
+//     id: number;
+// }
 function App() {
-    const [cells, setcells] = useState<ICell[][]>(makeCells());
+    const [cells, setcells] = useState<ICell[][]>(makeCells(8, 8, 10));
+    const [makeCellsParams, setMakeCellsParams] = useState([8, 8, 10]);
+    useEffect(() => {
+        reset();
+    }, [makeCellsParams]);
 
-    console.log(cells);
+    function reset() {
+        setcells(() =>
+            makeCells(
+                makeCellsParams[0],
+                makeCellsParams[1],
+                makeCellsParams[2]
+            )
+        );
+    }
+
+    function settingDif(y: number, x: number, mine: number) {
+        setMakeCellsParams(() => [y, x, mine]);
+    }
+
     return (
         <StyledMinesweeper>
             <h1>지뢰찾기</h1>
-            <div>재시작</div>
+            <div className="setDif">
+                <div onClick={() => settingDif(8, 8, 10)}>초급</div>
+                <div onClick={() => settingDif(16, 16, 40)}>중급</div>
+                <div onClick={() => settingDif(16, 32, 99)}>고급</div>
+            </div>
+            <div className="reset" onClick={reset}>
+                재시작
+            </div>
             <StyledTable>
                 {cells.map((y) => (
                     <tr>
@@ -33,6 +62,17 @@ const StyledMinesweeper = styled.div`
     height: 100vh;
     width: 100%;
     flex-direction: column;
+    .reset {
+        cursor: pointer;
+    }
+    .setDif {
+        div {
+            cursor: pointer;
+        }
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+    }
 `;
 
 const StyledTable = styled.table`
--- src/app/lib/func/funcs.ts
+++ src/app/lib/func/funcs.ts
@@ -17,29 +17,35 @@ function MakeRandomList(min: number, max: number, count: number) {
     return randomList;
 }
 
-function addMine(cells: ICell[][], y: number, x: number) {
+function addMine(cells: ICell[][], y: number, x: number, mine: number) {
     //랜덤 수 추출
-    const randomList = MakeRandomList(0, 63, 10);
-    console.log(randomList);
+    const randomList = MakeRandomList(0, y * x - 1, mine);
     //지뢰 추가
-    randomList.forEach((random) => {
-        const randomY = Math.floor(random / 8);
-        const randomX = random % 8;
-        cells[randomY][randomX].value = 9;
-        //주변 지뢰 갯수 추가
+    const cellsCopy = cells.map((row) => {
+        return row.map((cell) => {
+            return {
+                ...cell,
+                value: randomList.includes(cell.id) ? 9 : 0,
+            };
+        });
     });
-    //지뢰 갯수 추가
-    cells.forEach((row, y) => {
-        row.forEach((cell, x) => {
+
+    // 지뢰 갯수 추가
+
+    cellsCopy.forEach((row, yy) => {
+        row.forEach((cell, xx) => {
             if (cell.value > 8) {
+                //지뢰라면
                 //주변 지뢰 갯수 추가
-                for (let i = y - 1; i <= y + 1; i++) {
-                    for (let j = x - 1; j <= x + 1; j++) {
-                        if (i < 0 || j < 0 || i > 7 || j > 7) {
+                for (let i = yy - 1; i <= yy + 1; i++) {
+                    for (let j = xx - 1; j <= xx + 1; j++) {
+                        if (i < 0 || j < 0 || i > y - 1 || j > x - 1) {
                             continue;
                         }
-                        if (cells[i][j].value < 9) {
-                            cells[i][j].value++;
+                        console.log(i, j);
+
+                        if (cellsCopy[i][j].value < 9) {
+                            cellsCopy[i][j].value++;
                         }
                     }
                 }
@@ -47,21 +53,20 @@ function addMine(cells: ICell[][], y: number, x: number) {
         });
     });
 
-    return cells;
+    return cellsCopy;
 }
 
-function makeCells() {
-    let y = 8;
-    let x = 8;
+function makeCells(y: number, x: number, mine: number) {
     let id = 0;
     //셀 생성
-    const cells = [...Array(y)].map(() => {
+    let cells = [...Array(y)].map(() => {
         return [...Array(x)].map(() => {
             return { id: id++, flag: false, visible: false, value: 0 };
         });
     });
 
-    addMine(cells, 0, 0);
+    cells = addMine(cells, y, x, mine);
+    console.log(cells);
 
     return cells;
 }
