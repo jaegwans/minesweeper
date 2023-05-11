34a756fc2ecac0050fc8dda8bb9cd625b228237f
jaegwans <skhu201811140@gmail.com>
2023-05-12 01:12:21 +0900

    chore: 주석 작업

--- src/app/minesweeper/minesweeperSlice.ts
+++ src/app/minesweeper/minesweeperSlice.ts
@@ -39,13 +39,12 @@ export const minesweeperSlice = createSlice({
             // 클릭시 셀 업데이트 (숫자만)
             const { id }: { id: number } = action.payload;
 
-            const stateCopy = JSON.parse(JSON.stringify(state));
-            // const stateCopy = { state.map((y) =>[ y.map((x) => x)) };
-            //깊은 복사 2층
+            const stateCopy = JSON.parse(JSON.stringify(state)); //깊은 복사 2층 / 느린 방법이었고 더 좋은 방법이 있다고 생각함
+
             stateCopy.cells.map((y: any[]) =>
                 y.map((x) => {
                     if (x.id === id) {
-                        x.visible = true;
+                        x.visible = true; // 클릭한 셀 visible true
                     }
                     return x;
                 })
@@ -54,12 +53,11 @@ export const minesweeperSlice = createSlice({
             return stateCopy;
         },
         handelClickZeroCell: (state, action) => {
-            const cell = action.payload;
-            const { id }: { id: number } = cell;
-            const stateCopy = JSON.parse(JSON.stringify(state));
-            let zeroCellXY: any = { x: 0, y: 0 };
-
-            console.log(stateCopy.cells);
+            // 클릭한 셀이 0일 경우 셀 포함 주변 셀 업데이트
+            const cell = action.payload; // 클릭한 셀
+            const { id }: { id: number } = cell; // 클릭한 셀 id
+            const stateCopy = JSON.parse(JSON.stringify(state)); //깊은 복사 2층
+            let zeroCellXY: any = { x: 0, y: 0 }; // 클릭한 셀의 좌표 를 담을 변수
 
             // xy 좌표 추출
             stateCopy.cells.map((y: any[]) =>
@@ -70,28 +68,38 @@ export const minesweeperSlice = createSlice({
                     }
                 })
             );
+
             console.log(zeroCellXY);
 
-            const visited = [];
+            const visited = []; // 방문한 셀을 담을 배열
 
-            const { x, y } = zeroCellXY;
-            const { cells } = stateCopy;
-            const queue = [{ x: x, y: y }];
-            visited.push({ x, y });
-
-            console.log(queue);
+            const { x, y } = zeroCellXY; // 클릭한 셀의 좌표
+            const { cells } = stateCopy; // 셀들
+            const queue = [{ x: x, y: y }]; // 방문할 셀을 담을 큐
+            visited.push({ x, y }); // 방문한 셀을 담음
 
             while (queue.length > 0) {
-                const { x, y } = queue.shift() as any;
+                // 큐가 빌때까지 반복
+                const { x, y } = queue.shift() as any; // 큐에서 셀을 꺼냄
                 for (let i = y - 1; i <= y + 1; i++) {
                     for (let j = x - 1; j <= x + 1; j++) {
-                        if (i < 0 || j < 0 || i > 7 || j > 7) {
+                        if (
+                            //범위를 벗어날 경우 continue
+                            i < 0 ||
+                            j < 0 ||
+                            i > state.gameInfo.y - 1 ||
+                            j > state.gameInfo.x - 1
+                        ) {
                             continue;
                         }
                         if (cells[i][j].value === 0) {
+                            // 빈칸이면
+
                             if (!visited.some((v) => v.x === j && v.y === i)) {
-                                queue.push({ x: j, y: i });
-                                visited.push({ x: j, y: i });
+                                // 현 방문 셀이 아니면
+
+                                queue.push({ x: j, y: i }); //큐에 넣고
+                                visited.push({ x: j, y: i }); // 방문한 셀에 넣음
                             }
                         }
                     }
